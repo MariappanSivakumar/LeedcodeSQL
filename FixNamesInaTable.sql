55c1fd75eebbb070c29f32d694aac5dc1806661c
Mariappan <97144165+MariappanSivakumar@users.noreply.github.com>
2022-10-25 20:48:46 +0530

    Create FixNamesInaTable.sql

---
+++ FixNamesInaTable.sql
@@ -0,0 +1,20 @@
+/*
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+*/
+SELECT user_id,CONCAT(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2,LENGTH(name))))name FROM Users ORDER BY user_id;
