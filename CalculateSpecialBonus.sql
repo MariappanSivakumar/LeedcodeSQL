403f4b8824e831877f4d55e2ca75c09de7b18f1e
Mariappan <97144165+MariappanSivakumar@users.noreply.github.com>
2022-10-25 20:26:54 +0530

    Create CalculateSpecialBonus.sql

---
+++ CalculateSpecialBonus.sql
@@ -0,0 +1,27 @@
+/*
+Example 1:
+
+Input: 
+Employees table:
++-------------+---------+--------+
+| employee_id | name    | salary |
++-------------+---------+--------+
+| 2           | Meir    | 3000   |
+| 3           | Michael | 3800   |
+| 7           | Addilyn | 7400   |
+| 8           | Juan    | 6100   |
+| 9           | Kannon  | 7700   |
++-------------+---------+--------+
+Output: 
++-------------+-------+
+| employee_id | bonus |
++-------------+-------+
+| 2           | 0     |
+| 3           | 0     |
+| 7           | 7400  |
+| 8           | 0     |
+| 9           | 7700  |
++-------------+-------+
+*/
+SELECT employee_id, IF(employee_id%2=1 AND name NOT LIKE 'M%' , salary ,0)bonus
+ FROM Employees ORDER BY 1;
